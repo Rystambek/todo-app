e08673ad5dcd0eeba815b9b1593862bb18192cc2
Rystambek <rustambekjumaqulov@gmail.com>
2023-06-13 19:24:01 +0500

    all views

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import TaskListView,TaskIdView,TaskDoneView,TaskUndoneView
 
 urlpatterns = [
     path("tasks/", TaskListView.as_view()),
-    path('task/<int:id>',TaskIdView.as_view()),
-    path('task/<int:id>/done',TaskDoneView.as_view()),
-    path('task/<int:id>/undone',TaskUndoneView.as_view())
+    path('tasks/<int:id>',TaskIdView.as_view()),
+    path('tasks/<int:id>/done',TaskDoneView.as_view()),
+    path('tasks/<int:id>/undone',TaskUndoneView.as_view())
 ]
