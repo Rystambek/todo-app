fac16945905c4620a2e4c0ab33739da530bc671f
Rystambek <rustambekjumaqulov@gmail.com>
2023-06-13 19:21:33 +0500

    all views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView,TaskIdView,TaskDoneView,TaskUndoneView
 
 urlpatterns = [
     path("tasks/", TaskListView.as_view()),
+    path('task/<int:id>',TaskIdView.as_view()),
+    path('task/<int:id>/done',TaskDoneView.as_view()),
+    path('task/<int:id>/undone',TaskUndoneView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.views import View
-from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse,HttpRequest
 from .models import Task
+import json
 
 def to_dict(task: Task) -> dict:
     return {
@@ -14,6 +16,88 @@ def to_dict(task: Task) -> dict:
 
 
 class TaskListView(View):
-    def get(self, request):
+    def get(self, request:HttpRequest):
         tasks = Task.objects.all()
         return JsonResponse([to_dict(task) for task in tasks], safe=False, status=200)
+    def post(self,request:HttpRequest):
+        data_json = request.body.decode()
+        data = json.loads(data_json)
+
+        if not data.get('title'):
+            return JsonResponse({'status':"title yo'q"})
+        elif not data.get('description'):
+            return JsonResponse({'status':'description yo\'q'})
+        
+        task = Task.objects.create(
+            title = data['title'],
+            description = data['description']
+        )
+
+        task.save()
+
+        return JsonResponse(to_dict(task),status=201)
+
+class TaskIdView(View):
+    def get(self,request:HttpRequest,id):
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'object does not exist!'})
+        task = Task.objects.get(id=id)
+        return JsonResponse(to_dict(task),status=200)
+    def put(self,request:HttpRequest,id):
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'object does not exist!'})
+        
+        data_json = request.body.decode()
+        data = json.loads(data_json)
+
+        if data.get('title'):
+            task.name = data['title']
+        if data.get('description'):
+            task.website = data['description']
+       
+
+        task.save()
+
+        return JsonResponse(to_dict(task),status=200)
+    
+    def delete(self,request:HttpRequest,id) -> JsonResponse:
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'object does not exist!'})
+
+        task.delete()
+
+        return JsonResponse({'status': 'ok'},status=204)
+        
+class TaskDoneView(View):
+    def post(self,request:HttpRequest,id):
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'object does not exist!'})
+        
+        task.done = True
+       
+
+        task.save()
+
+        return JsonResponse(to_dict(task),status=200)
+    
+class TaskUndoneView(View):
+    def post(self,request:HttpRequest,id):
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'object does not exist!'})
+        
+        task.done = False
+       
+
+        task.save()
+
+        return JsonResponse(to_dict(task),status=200)
