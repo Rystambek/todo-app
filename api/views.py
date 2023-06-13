3e0a049f95a9d87a9af4a22d57f9904fc1f2446e
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-13 18:52:27 +0500

    get all tasks

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import Task
 
-# Create your views here.
+def to_dict(task: Task) -> dict:
+    return {
+        "id": task.id,
+        "title": task.title,
+        "description": task.description,
+        "done": task.done,
+        "created": task.created,
+        "updated": task.updated,
+    }
+
+
+class TaskListView(View):
+    def get(self, request):
+        tasks = Task.objects.all()
+        return JsonResponse([to_dict(task) for task in tasks], safe=False, status=200)
